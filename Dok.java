94cca8745dd7e57e00a1d417bb9f678d06356675
evgens12348 <evgen.s12348@gmail.com>
2017-04-18 11:57:00 +0300

    Add files via upload

--- Dok.java
+++ Dok.java
@@ -3,52 +3,37 @@ package com.gmail.s12348.evgen;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class Dok {
-	private RandomStart rs;
+public class Dok {	
 
 	public Dok() {
 
 	}
-
-	public RandomStart randomStart() {
-		int a;
-		int b;
-		int c;
-		do {
-			a = (int) (Math.random() * 100);
-			b = (int) (Math.random() * 100);
-			c = (int) (Math.random() * 100);
-		} while (a == b & b == c & c == a);
-		this.rs = new RandomStart(a, b, c);
-		return rs;
-
-	}
-
+	
 	public void turnOfShips() {
 
 		ExecutorService exs = Executors.newFixedThreadPool(2);
-		randomStart();
-		if (rs.getA() < rs.getB() & rs.getB() < rs.getC()) {
+		int a = (int) (Math.random() * 100);		
+		if (a>=0&a<=17) {
 			exs.submit(new ShipTree());
 			exs.submit(new ShipTwo());
 			exs.submit(new ShipOne());
-		} else if (rs.getA() < rs.getC() & rs.getC() < rs.getB()) {
+		} else if (a>17&a<=34) {
 			exs.submit(new ShipTwo());
 			exs.submit(new ShipTree());
 			exs.submit(new ShipOne());
-		} else if (rs.getB() < rs.getA() & rs.getA() < rs.getC()) {
+		} else if (a>34&a<=51) {
 			exs.submit(new ShipTree());
 			exs.submit(new ShipOne());
 			exs.submit(new ShipTwo());
-		} else if (rs.getB() < rs.getC() & rs.getC() < rs.getA()) {
+		} else if (a>51&a<=68) {
 			exs.submit(new ShipOne());
 			exs.submit(new ShipTree());
 			exs.submit(new ShipTwo());
-		} else if (rs.getC() < rs.getA() & rs.getA() < rs.getB()) {
+		} else if (a>68&a<=85) {
 			exs.submit(new ShipTwo());
 			exs.submit(new ShipOne());
 			exs.submit(new ShipTree());
-		} else if (rs.getC() < rs.getB() & rs.getB() < rs.getA()) {
+		} else if (a>85) {
 			exs.submit(new ShipOne());
 			exs.submit(new ShipTwo());
 			exs.submit(new ShipTree());
@@ -57,30 +42,5 @@ public class Dok {
 
 	}
 
-	private class RandomStart {
-		private int a;
-		private int b;
-		private int c;
-
-		public RandomStart(int a, int b, int c) {
-			super();
-			this.a = a;
-			this.b = b;
-			this.c = c;
-		}
-
-		public int getA() {
-			return a;
-		}
-
-		public int getB() {
-			return b;
-		}
-
-		public int getC() {
-			return c;
-		}
-
-	}
 
 }
